5578330de5b84eb0e0ac48c7a26c7d97d8dc68a4
JulioLaz <julioalazarte@gmail.com>
2024-11-12 11:39:42 -0300

    stars

--- app.py
+++ app.py
@@ -11,8 +11,19 @@ app = Flask(__name__)
 analyzer = OptimizedSentimentAnalyzer.load_model('optimized_sentiment_model_2_20000_11_Nov_2024_20hs_11min.pkl')
 vocab = analyzer.vectorizer.vocabulary_
 
+def classify_with_stars(value):
+    if not 0 <= value <= 100:
+        return None  # Return None for values outside the range
+    if value <= 20: stars = 1 
+    elif value <= 40: stars = 2 
+    elif value <= 60: stars = 3 
+    elif value <= 80: stars = 4 
+    else: stars = 5
+    return "⭐" * stars
+
 @app.route('/', methods=['GET', 'POST'])
 def analyze_sentiment():
+    
     if request.method == 'POST':
         start_time = time.time()
         
@@ -27,8 +38,8 @@ def analyze_sentiment():
             translated_text = 'Lo siento, tengo problemas para traducir este texto.'
         
         # Get Gemini analysis
-      #   analyzer_gemini = '50% positive, 50% negative'  # Replace with actual gemini.chat(text)
-        analyzer_gemini = gemini.chat(text)
+        analyzer_gemini = '40% positive, 60% negative'  # Replace with actual gemini.chat(text)
+      #   analyzer_gemini = gemini.chat(text)
 
         print('analyzer_gemini:', analyzer_gemini)
         
@@ -42,6 +53,7 @@ def analyze_sentiment():
             parts = analyzer_gemini.split(',')
             positive_gemini = int(parts[0].split('%')[0].strip())
             negative_gemini = int(parts[1].split('%')[0].strip())
+            start_genimi= classify_with_stars(positive_gemini)
         else:
             none_gemini = 'None'
             positive_gemini = 0
@@ -51,6 +63,8 @@ def analyze_sentiment():
         if filtered_text:
             result = analyzer.predict(filtered_text)
             print('result:', result['probabilities']['positive'])
+            start_sgd= classify_with_stars(result['probabilities']['positive']*100)
+
             response_data = {
                 'filtered_text': filtered_text,
                 'result': result,
@@ -61,7 +75,9 @@ def analyze_sentiment():
                 'translated_text': translated_text,
                 'gemini_result': analyzer_gemini,
                 'sentiment': 'unknown',
-                'none_gemini': none_gemini
+                'none_gemini': none_gemini,
+                'start_genimi':start_genimi,
+                'start_sgd':start_sgd
             }
         else:
             print('filtered_text:', filtered_text)
@@ -75,6 +91,8 @@ def analyze_sentiment():
                 'sentiment': 'unknown',
                 'positive': 0,
                 'negative': 0,
+                'start_sgd': 0,
+                'start_genimi': 0,
                 'gemini_result': analyzer_gemini
             }
         
